869173642a6cf965c40d0ec976d3ad2e81f26d25
leehua <leehua095.gmail.com>
2025-05-18 21:33:28 +0800

    改变更新文本

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -48,8 +48,8 @@ if(navigator.serviceWorker){
     if(data.command == "UPDATE_FOUND"){
       console.log("UPDATE_FOUND_BY_SW", data);
       createSnackbar({
-        message: "Content updated.",
-        actionText:"refresh",
+        message: "煮啵更新啦！点击右键查看更新->",
+        actionText:"更新",
         action: function(e){location.reload()}
       })
     }
